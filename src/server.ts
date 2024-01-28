7b9f852f2a9bd9785430e4fca63ec353c9ac1132
giga <tacuara10@gmail.com>
2024-01-28 21:08:55 +0000

    still building payload / express server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,28 @@
 import express from "express"
+import { getPayloadClient } from "./get-payload"
+import { Payload } from "payload"
+import { nextApp, nextHandler } from "./next-utils"
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
 
-const start = async () => {
+async function start () {
     const payload = await getPayloadClient()
+    initOptions: {
+        express: app
+        onInit: async (cms: Payload) => {
+            cms.logger.info(`Admin URL ${cms.getAdminURL()}`)
+        }
+    }
 
+    app.use((req, res) => nextHandler(req, res))
+
+    nextApp.prepare().then(() => {
+        payload.logger.ino("Next.js started")
+
+        app.listen(PORT, async () => {
+            payload.logger.ingo(`Next.js App URL: ${process.env.NEXT_PUBLIC_APP_URL}`)})
+        }) 
 }
 
 start ()
